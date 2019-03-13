3d7dc451905ffb647ff7ed239b453a4618006feb
Eric Groß <ericgross.eg@gmail.com>
2019-03-13 16:44:23 +0100

    Add everything that needs to be done in this course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {COURSES} from '../db-data';
+import {Course} from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,20 @@ import {COURSES} from '../db-data';
 })
 export class AppComponent {
 
+    courses = COURSES;
 
+    title = COURSES[1].description;
 
+    startDate = new Date(2000, 0, 1);
+
+    price = 9.99239472394;
+
+    rate = 0.67;
+
+    course = COURSES[1];
+
+  onCourseSelected(course: Course) {
+
+    console.log('App Component - click event bubbled...', course);
+  }
 }
